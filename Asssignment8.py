66e5f4de38177c13884cbee3847074881301e399
sarehnaderi <140264213+sarehnaderi@users.noreply.github.com>
2023-08-19 00:00:35 +0330

    Update Asssignment8.py

--- Asssignment8.py
+++ Asssignment8.py
@@ -1,13 +1,12 @@
-
 products = []
-
 def read_data():
     f = open("text.txt","r")
     for line in f:
         product = line.split(",") 
-        dic = {"id":product[0],"name":product[1],"price":product[2],"count":product[3]}
+        dic = {"id":product[0],"name":product[1],"unit price":product[2],"count":product[3]}
         products.append(dic)
-    print(products)
+    # print(products)
+
 def show_menu():
     print("*Menu*")
     print("1-add")
@@ -23,9 +22,10 @@ def add():
     name = input("Enter name:")
     price = input("Enter price:")
     count = input("Enter count:")
-    dic = {'id':id, 'name':name, 'price':price, 'count':count}
+    dic = {'id':id, 'name':name, 'unit price':price, 'count':count}
     products.append(dic) 
     print (products)
+
 def delete():
     product_id = input("Enter the ID of product that you want to delete: ")
     for product in products:
@@ -47,43 +47,48 @@ def search():
             break
     else: 
         print("Not found")
+        
 def buy():
+    receipt = []
+    total_price = 0
     while True:
-        print("Choose your required service(1-3):")
+        print(" ")
+        print("** Choose your required service(1-3):")
         print("1-Add a new product to your buy list")
         print("2-Recieve your receipt")
         print("3-Show menu")
+        print(" ")
         option = int(input("Enter your choice:"))
-        # انتخاب کالا
         if option == 1:
-            product_id = input("Enter product ID::")
+            product_id = int(input("Enter product ID::"))
             for product in products:
-                    if product_id == product['id']:
+                    if product_id == int(product['id']):
                         wanted_count = int(input("Enter the count of the product that you need:"))
                         pro_count = int(product['count'])
                         if wanted_count <= pro_count:
                             remain = pro_count - wanted_count
-                            print("Your item(s) added to your buy list.")
+                            product['count'] = wanted_count
+                            print(product)
+                            print("**Your item(s) added to your buy list**")
                             print("Remaining count:",remain)
-                            print("id \t name \t price")
-                            print(product['id'],'\t',product['name'],'\t',product['price'])
-                      
+                            receipt.append(product)
+                            subtotal = int(product['count']) * int(product['unit price'])
+                            total_price += subtotal
                         else:
-                            print("Sorry! There is not enough of this item available.")
-                            print("Available count of your required item is:",product['count'])
+                            print("__Sorry! There is not enough of this item available__")
+                            print("__Available count of your required item is:",product['count'])
                         break
-                    else:
-                        print("The ID is not defined.")
-                        break
-        # دریافت رسید
+            else:
+                print("The ID is not defined.")
+
         elif option == 2:
-            receipt = []
-            dic = {'id':product['id'],'name':product['name'],'price':product['price']}
-            receipt.append(dic)
-            print("id \t name \t price")
-            for p in receipt:
-                print(p['id'],'\t',p['name'],'\t',p['price'])
-        
+            print("**Here is your receipt**")
+            print("id \t name \t unit price \t count")
+            for product in receipt:
+                print(product['id'],'\t',product['name'],'\t',product['unit price'],'\t',product['count'])
+            print(">>>Total price:",total_price)
+            break
+
         elif option == 3:
             show_menu()
             user_choice = int(input("Enter your choise:"))
@@ -105,8 +110,6 @@ def buy():
                 print("Error! Enter a valid number(1-7).")
             break
         
-           
-
 def edit():
     product_id = input("Enter the ID of product that you want to edit:")
     for product in products:
@@ -121,12 +124,11 @@ def edit():
                 edit_name = input("Enter new name:")
                 product['name'] = edit_name
                 print("Your list is updated")
-                print("Your updated product info:",product)
+                print("Done! Your updated product info:",product)
             elif choice == '2':
                 edit_price = input("Enter updated price:")
-                print("*")
-                product['price'] = edit_price
-                print("Your updated product info:",product)
+                product['unit price'] = edit_price
+                print("Done! Your updated product info:",product)
             elif choice == '3':
                 edit_count = input("Enter updated count:")
                 product['count'] = edit_count
@@ -135,14 +137,13 @@ def edit():
                 print("Error! Enter a valid number(1-3).")
                 print(products)
                 
-            
 def exit():
     pass
 
 def show_products():
-    print("id \t name \t price \t count")
+    print("id \t name \t unit price \t count")
     for product in products:
-        print(product['id'],'\t',product['name'],'\t',product['price'],'\t',product['count'])
+        print(product['id'],'\t',product['name'],'\t',product['unit price'],'\t',product['count'])
        
 read_data()
 show_menu()
